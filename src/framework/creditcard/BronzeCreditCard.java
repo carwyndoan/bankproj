81fa57e3b77c1a64030b2b5690fb94efa323ccd9
phuocdoan <ddoan@miu.edu>
2020-11-18 09:53:51 -0600

    Update creditcard interest

--- src/framework/creditcard/BronzeCreditCard.java
+++ src/framework/creditcard/BronzeCreditCard.java
@@ -4,11 +4,11 @@ public class BronzeCreditCard  implements CreditCardStrategyInterface {
 
     @Override
     public double monthlyInterest() {
-        return 0;
+        return 0.1;
     }
 
     @Override
     public double minimumPayment() {
-        return 0;
+        return 0.14;
     }
 }
--- src/framework/creditcard/SilverCreditCard.java
+++ src/framework/creditcard/SilverCreditCard.java
@@ -4,11 +4,11 @@ public class SilverCreditCard implements CreditCardStrategyInterface {
 
     @Override
     public double monthlyInterest() {
-        return 0;
+        return 0.08;
     }
 
     @Override
     public double minimumPayment() {
-        return 0;
+        return 0.12;
     }
 }
