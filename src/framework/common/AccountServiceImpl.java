5567b9f56aa8c15179b42cb258795d0614963b82
Thai Nguyen <ndt.rachkien@gmail.com>
2020-11-17 20:33:20 -0600

    add

--- src/framework/common/AccountServiceImpl.java
+++ src/framework/common/AccountServiceImpl.java
@@ -9,6 +9,7 @@ import framework.creditcard.GoldCreditCard;
 import framework.creditcard.SilverCreditCard;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class AccountServiceImpl implements AccountService {
 
@@ -109,6 +110,15 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public void calculateInterest() {
-		getAllAccounts().forEach(Account::calculateInterest);
+		getAllAccounts().stream().
+				filter(account -> account.getInterestCalculation() != null)
+				.collect(Collectors.toList()).forEach(Account::calculateInterest);
+	}
+
+	@Override
+	public void calculateCCInterest() {
+		getAllAccounts().stream().
+				filter(account -> account.getCcinterestCalculation() != null)
+				.collect(Collectors.toList()).forEach(Account::calculateInterest);
 	}
 }
