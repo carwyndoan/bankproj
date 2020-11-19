154fef2d6f3563cb2378e4c19dd6ffe239398179
phuocdoan <ddoan@miu.edu>
2020-11-19 11:20:09 -0600

    Change email

--- src/framework/common/AccountServiceImpl.java
+++ src/framework/common/AccountServiceImpl.java
@@ -21,50 +21,50 @@ public class AccountServiceImpl implements AccountService {
 
 	private void generateSample(AccountDAO accountDAO){
 		if(getAllAccounts().size() <= 0) {
-			Account account = createAccount("1-111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			Account account = createAccount("1-111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.CHECKING);
 			account.setInterestCalculation(new PersonalChekingInterestCalculation());
 			account.deposit(79797979);
 			Customer customer = account.getCustomer();
 
-			account = createCreditCard("9-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("9-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.GOLD);
 			account.setCcinterestCalculation(new GoldCreditCard());
 			account.setCustomer(customer);
 			((CreditCard)account).setExpiryDate(LocalDate.of(2020,10,1));
 
-			account = createCreditCard("8-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("8-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.SILVER);
 			account.setCcinterestCalculation(new SilverCreditCard());
 			account.setCustomer(customer);
 			((CreditCard)account).setExpiryDate(LocalDate.of(2020,9,1));
 
-			account = createCreditCard("7-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("7-1111", "Duc Phuoc Doan", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.BRONZE);
 			account.setCcinterestCalculation(new BronzeCreditCard());
 			account.setCustomer(customer);
 			((CreditCard)account).setExpiryDate(LocalDate.of(2020,8,1));
 
 			///
-			account = createAccount("2-111", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createAccount("2-111", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.SAVING);
 			account.deposit(39393939);
 			account.setInterestCalculation(new PersonalSavingInterestCalculation());
 			customer = account.getCustomer();
 
-			account = createCreditCard("9-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("9-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.GOLD);
 			account.setCcinterestCalculation(new GoldCreditCard());
 			account.setCustomer(customer);
 			((CreditCard)account).setExpiryDate(LocalDate.of(2020,5,1));
 
-			account = createCreditCard("8-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("8-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.SILVER);
 			account.setCcinterestCalculation(new SilverCreditCard());
 			account.setCustomer(customer);
 			((CreditCard)account).setExpiryDate(LocalDate.of(2020,4,1));
 
-			account = createCreditCard("7-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "ddoan@miu.edu");
+			account = createCreditCard("7-1112", "Duy Thai Nguyen", "4th", "Fairfield", "IA", "52557", "noreply@miu.edu");
 			account.setAccountType(AccountType.BRONZE);
 			account.setCcinterestCalculation(new BronzeCreditCard());
 			account.setCustomer(customer);
